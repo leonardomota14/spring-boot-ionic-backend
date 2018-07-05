6d65e0979e8c480095982191f919830e3a4a93fb
Leonardo Mota <leonardomota14@gmail.com>
2018-07-05 16:15:44 -0300

    end point/clientes/{id} disponivel

---
+++ src/main/java/com/leonardomota/cursomc/services/ClienteService.java
@@ -0,0 +1,23 @@
+package com.leonardomota.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardomota.cursomc.domain.Cliente;
+import com.leonardomota.cursomc.repositories.ClienteRepository;
+import com.leonardomota.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
+	}
+}
