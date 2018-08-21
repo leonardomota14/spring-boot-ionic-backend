407a4d4c2e40eaaf3264ab25bff7ce6bfca2d61a
L460 <L460@DESKTOP-UGM43Q5>
2018-08-20 22:05:05 -0300

    inserindo um novo cliente com POST

--- src/main/java/com/leonardomota/cursomc/resources/ClienteResource.java
+++ src/main/java/com/leonardomota/cursomc/resources/ClienteResource.java
@@ -19,6 +19,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.leonardomota.cursomc.domain.Cliente;
 import com.leonardomota.cursomc.dto.ClienteDTO;
+import com.leonardomota.cursomc.dto.ClienteNewDTO;
 import com.leonardomota.cursomc.services.ClienteService;
 
 @RestController
@@ -35,7 +36,7 @@ public class ClienteResource {
 	}
 	
 	@RequestMapping(method=RequestMethod.POST)
-	public ResponseEntity<Void> inserir(@Valid @RequestBody ClienteDTO objDto) {
+	public ResponseEntity<Void> inserir(@Valid @RequestBody ClienteNewDTO objDto) {
 		Cliente obj = service.fromDTO(objDto);
 		obj = service.insert(obj);
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
--- src/main/java/com/leonardomota/cursomc/services/ClienteService.java
+++ src/main/java/com/leonardomota/cursomc/services/ClienteService.java
@@ -9,10 +9,16 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.leonardomota.cursomc.domain.Cidade;
 import com.leonardomota.cursomc.domain.Cliente;
+import com.leonardomota.cursomc.domain.Endereco;
+import com.leonardomota.cursomc.domain.enums.TipoCliente;
 import com.leonardomota.cursomc.dto.ClienteDTO;
+import com.leonardomota.cursomc.dto.ClienteNewDTO;
 import com.leonardomota.cursomc.repositories.ClienteRepository;
+import com.leonardomota.cursomc.repositories.EnderecoRepository;
 import com.leonardomota.cursomc.services.exceptions.DataIntegratyException;
 import com.leonardomota.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -21,6 +27,8 @@ public class ClienteService {
 	
 	@Autowired
 	private ClienteRepository repo;
+	@Autowired
+	private EnderecoRepository enderecoRepo;
 	
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -28,9 +36,12 @@ public class ClienteService {
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Cliente.class.getName()));
 	}
 	
+	@Transactional
 	public Cliente insert(Cliente obj) {
 		obj.setId(null);
-		return repo.save(obj);
+		obj = repo.save(obj);
+		enderecoRepo.saveAll(obj.getEnderecos());
+		return obj;
 	}
 	
 	public Cliente update(Cliente obj) {
@@ -57,8 +68,24 @@ public class ClienteService {
 		return repo.findAll(pageRequest);
 	}
 	
-	public Cliente fromDTO(ClienteDTO cliDto) {
-		return new Cliente(cliDto.getId(), cliDto.getNome(), cliDto.getEmail(), null, null);
+	public Cliente fromDTO(ClienteDTO objDto) {
+		return new Cliente(objDto.getId(), objDto.getNome(), objDto.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO objDto) {
+		Cliente cli = new Cliente(null, objDto.getNome(), objDto.getEmail(), objDto.getCpfOuCnpj(), TipoCliente.toEnum(objDto.getTipo()));
+		Cidade cid = new Cidade(objDto.getCidadeId(), null, null);
+		Endereco end = new Endereco(null, objDto.getLogradouro(), objDto.getNumero(), objDto.getComplemento(), objDto.getBairro(), objDto.getCep(), cli, cid);
+		cli.getEnderecos().add(end);
+		cli.getTelefones().add(objDto.getTelefone1());
+		if(objDto.getTelefone2() != null) {
+			cli.getTelefones().add(objDto.getTelefone2());
+		}
+		if(objDto.getTelefone3() != null) {
+			cli.getTelefones().add(objDto.getTelefone3());
+		}
+		
+		return cli;
 	}
 	
 	private void updateData(Cliente newObj, Cliente obj) {
