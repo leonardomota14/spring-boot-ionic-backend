9456031031e108e3c36898045913f72e8cdfa1af
Leonardo Mota <Leonardo Mota@LeonardoMota-PC>
2018-09-01 23:23:29 -0300

    Inserindo novo pedido

--- src/main/java/com/leonardomota/cursomc/domain/PagamentoComCartao.java
+++ src/main/java/com/leonardomota/cursomc/domain/PagamentoComCartao.java
@@ -2,9 +2,11 @@ package com.leonardomota.cursomc.domain;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonTypeName;
 import com.leonardomota.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
+@JsonTypeName("pagamentoComCartao")
 public class PagamentoComCartao extends Pagamento {
 	private static final long serialVersionUID = 1L;
 	
