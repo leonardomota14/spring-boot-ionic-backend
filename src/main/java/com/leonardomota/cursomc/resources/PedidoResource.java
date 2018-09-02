93eccf4eb419c306dfbb342a4f9abe7dfdcb012c
Leonardo Mota <Leonardo Mota@LeonardoMota-PC>
2018-09-01 23:25:25 -0300

    ajustando imports

--- src/main/java/com/leonardomota/cursomc/resources/PedidoResource.java
+++ src/main/java/com/leonardomota/cursomc/resources/PedidoResource.java
@@ -13,9 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.leonardomota.cursomc.domain.Categoria;
 import com.leonardomota.cursomc.domain.Pedido;
-import com.leonardomota.cursomc.dto.CategoriaDTO;
 import com.leonardomota.cursomc.services.PedidoService;
 
 @RestController
