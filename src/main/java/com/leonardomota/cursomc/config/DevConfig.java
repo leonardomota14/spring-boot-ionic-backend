9b107cca8ac1591b43d84b1cf9bed81d8cdcc183
leonardo_mota <leonardomota14@gmail.com>
2018-09-05 23:48:03 -0300

    Implementando SmtpEmailService com servidor do Google

--- src/main/java/com/leonardomota/cursomc/config/DevConfig.java
+++ src/main/java/com/leonardomota/cursomc/config/DevConfig.java
@@ -9,6 +9,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import com.leonardomota.cursomc.services.DBService;
+import com.leonardomota.cursomc.services.EmailService;
+import com.leonardomota.cursomc.services.SmtpMailService;
 
 @Configuration
 @Profile("dev")
@@ -29,6 +31,11 @@ public class DevConfig {
 		
 		dbService.instantiateTestDatavase();		
 		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpMailService();
 	}
 	
 }
