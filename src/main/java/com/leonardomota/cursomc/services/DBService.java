e27c624a31aca8a9fce865bae9611f4a097796fa
leonardo_mota <leonardomota14@gmail.com>
2018-09-06 23:20:13 -0300

    Adicionando senha a cliente

--- src/main/java/com/leonardomota/cursomc/services/DBService.java
+++ src/main/java/com/leonardomota/cursomc/services/DBService.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.leonardomota.cursomc.domain.Categoria;
@@ -42,6 +43,7 @@ public class DBService {
 	@Autowired	private PedidoRepository pedidoRepository;
 	@Autowired	private PagamentoRepository pagamentoRepository;
 	@Autowired	private ItemPedidoRepository itemPedidoRepository;
+	@Autowired  private BCryptPasswordEncoder passwordEncoder;
 
 	public void instantiateTestDatavase() throws ParseException {
 		
@@ -107,7 +109,7 @@ public class DBService {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
-		Cliente cli1 = new Cliente(null, "Maria Silva", "contaprajogos0410@gmail.com", "36378912377", TipoCliente.PESSOAFISICA);
+		Cliente cli1 = new Cliente(null, "Maria Silva", "contaprajogos0410@gmail.com", "36378912377", TipoCliente.PESSOAFISICA, passwordEncoder.encode("12345"));
 		
 		cli1.getTelefones().addAll(Arrays.asList("27363323", "93838393"));
 		
