370abc8c0bc2b22cc519e7e84aac3e1ca64d3347
Leonardo Mota <leonardo@angulodigital.com.br>
2018-12-14 23:38:52 -0300

    ajustes para subir a producao

---
+++ src/main/java/com/leonardomota/cursomc/config/ProdConfig.java
@@ -0,0 +1,41 @@
+package com.leonardomota.cursomc.config;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.leonardomota.cursomc.services.DBService;
+import com.leonardomota.cursomc.services.EmailService;
+import com.leonardomota.cursomc.services.SmtpMailService;
+
+@Configuration
+@Profile("prod")
+public class ProdConfig {
+	
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		
+		if(!"create".equals(strategy)) {
+			return false;
+		}
+		
+		dbService.instantiateTestDatavase();		
+		return true;
+	}
+	
+	@Bean
+	public EmailService emailService() {
+		return new SmtpMailService();
+	}
+	
+}
